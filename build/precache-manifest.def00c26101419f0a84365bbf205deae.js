2fe8d3e60c733c9ac73761e26def01d88b9488ad
DanialKhan800 <72875993+DanialKhan800@users.noreply.github.com>
2023-06-09 14:45:49 +0000

    Build Created Successfully

---
+++ build/precache-manifest.def00c26101419f0a84365bbf205deae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b932887452d486af4a034c009d16b0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9b86db9d4c5a671520",
+    "url": "/static/css/main.848d8c40.chunk.css"
+  },
+  {
+    "revision": "ea67ae150c2254bd1ccf",
+    "url": "/static/js/2.77f01fbc.chunk.js"
+  },
+  {
+    "revision": "8a9b86db9d4c5a671520",
+    "url": "/static/js/main.a9c8595d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
